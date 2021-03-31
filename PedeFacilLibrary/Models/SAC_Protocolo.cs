909e5c70007c38afc48074f010fe77b06935c2e6
henriquerbf <henriquerbf@hotmail.com>
2021-03-30 22:31:00 -0300

    Incluindo Classe SAC_Protocolo (Fonte Fantasma)

---
+++ PedeFacilLibrary/Models/SAC_Protocolo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PedeFacilLibrary.Models
+{
+    public class SAC_Protocolo
+    {
+        [Key]
+        public int id_SAC_Protocolo { get; set; }
+
+        public int id_Entidade { get; set; }
+
+        public int id_Tipo { get; set; }
+
+        public DateTime dt_Abertura_Protocolo { get; set; }
+    }
+}
