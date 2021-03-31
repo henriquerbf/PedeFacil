90486d69ce43291ff4d127e0fd402b52d3594985
Bruno Silva <brunpersilva@gmail.com>
2021-03-30 21:14:39 -0300

    Added Log Class

---
+++ PedeFacilLibrary/Models/Log.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PedeFacilLibrary.Models
+{
+    public class Log
+    {
+        public int id_Log { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataHora { get; set; }
+    }
+}
